cbe36f1851d832bf4d179edc35eddf34d2d2a9f3
Ihor Biletskyi <ihor.biletskyi@optimizely.com>
2025-02-05 14:55:40 +0100

    Test project with the tag helper intellisense issue when tag helepr has custom tag, required attribute and parrent tag restriction

---
+++ TagHelpersIntelliSenseAttributesIssue/TestTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpersIntelliSenseAttributesIssue;
+
+[HtmlTargetElement("test", Attributes = "test")]
+public class TestTagHelper : TagHelper
+{
+    [HtmlAttributeName("test")]
+    public bool MyProperty { get; set; }
+}
+
+[HtmlTargetElement("test2", Attributes = "test2", ParentTag = "div")]
+public class TestTagHelper2 : TagHelper
+{
+    [HtmlAttributeName("test2")]
+    public bool MyProperty { get; set; }
+}
